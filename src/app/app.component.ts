c841c7d97929dfc879e8843f23dcd8a47624e55c
Ifeanyi Igbo <iigbo@cmcadd1.chemicalmarketing.com>
2017-08-15 17:10:06 -0400

    what can ngFor do for you

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  viewMode = 'map';
+  courses = [
+      { id: 1, name: 'course1'},
+      { id: 2, name: 'course2'},
+      { id: 3, name: 'course3'},
+  ];
+
+  loadCourses() {
+    this.courses = [
+      { id: 1, name: 'course1'},
+      { id: 2, name: 'course2'},
+      { id: 3, name: 'course3'},
+    ];
+  }
+
+  onAdd() {
+    this.courses.push({ id: 4, name: 'course4'});
+  }
+
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+
+  trackCourse(index, course){
+    return course ? course.id : undefined;
+  }
 }
